2bab78d73a2b0afbb3cfa08dda9865b68d25f78b
Timur Kazbekov <tkazbekov@gmail.com>
2018-11-13 16:56:34 +0600

    added cors headers

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const isProduction = process.env.NODE_ENV === 'production';
 const app = express();
 
 //Configure our app
-app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({limit: '50mb'}));
